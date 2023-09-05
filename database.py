f9015b0a157021de10bb13dfdd91a21330e3f20c
trillion2035 <79040454+trillion2035@users.noreply.github.com>
2023-09-05 17:59:21 +0000

    modified database.py with new planetscale credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-db_connection_string= "mysql+pymysql://2ft66lzpv5vuuuivua11:pscale_pw_HfHAwueJQsSOVsEtc463bm4SrbzJuevfnw3LjYUcDwu@aws.connect.psdb.cloud/careerwebsite?charset=utf8mb4"
+db_connection_string= "mysql+pymysql://z4l4xfs6ur3xb5p2ea7y:pscale_pw_J7SlUmiEiiEWrGoqVsaJsbBsXvISvCYpLmwRKvCyRT3@aws.connect.psdb.cloud/careerwebsite?charset=utf8mb4"
 
 engine = create_engine(
   db_connection_string,
@@ -10,3 +10,13 @@ engine = create_engine(
         }
   })
 
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    keys = result.keys()
+
+    jobs = []
+    for row in result.fetchall():
+        row_dict = dict(zip(keys, row))
+        jobs.append(row_dict)
+    return jobs
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from flask import Flask, render_template, jsonify
-from database import engine
+from database import load_jobs_from_db
 from sqlalchemy import text 
 
 app= Flask(__name__)
-
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    keys = result.keys()
-
-    jobs = []
-    for row in result.fetchall():
-        row_dict = dict(zip(keys, row))
-        jobs.append(row_dict)
-    return jobs
 
 @app.route("/")
 def hello_career():
@@ -23,7 +12,8 @@ def hello_career():
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
   
 
 if __name__ == "__main__":
