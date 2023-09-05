1d6cfaabbfa86afe49bc55b6e2398d123c48db54
trillion2035 <79040454+trillion2035@users.noreply.github.com>
2023-09-05 21:18:51 +0000

    edited database.py load_job_from_db function

--- database.py
+++ database.py
@@ -24,12 +24,15 @@ def load_jobs_from_db():
 
 
 def load_job_from_db(id):
+
+  
   with engine.connect() as conn:
     result=conn.execute(
       text("select * from jobs where id = :val"),
       val=id
     )
-    rows = result.fetchall()
+    
+    rows = result.all()
     if len(rows)==0:
       return None
     else:
