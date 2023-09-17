0b323f6b14d35415b43c0982c0cf9f14bc25fa5e
trillion2035 <79040454+trillion2035@users.noreply.github.com>
2023-09-17 13:28:48 +0000

    store applications in database

--- database.py
+++ database.py
@@ -1,15 +1,13 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string= os.environ['DB_CONNECTION_STRING']
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-     "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem",
-        }
-  })
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem",
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -18,19 +16,56 @@ def load_jobs_from_db():
 
     jobs = []
     for row in result.fetchall():
-        row_dict = dict(zip(keys, row))
-        jobs.append(row_dict)
+      row_dict = dict(zip(keys, row))
+      jobs.append(row_dict)
     return jobs
 
 
 def load_job_from_db(id):
-    with engine.connect() as conn:
-        s = text("select * from jobs where id = :val")
-        result = conn.execute(s, {"val": id})
-        row = result.fetchone()
-        
-        if row is None:
-            return None
+  with engine.connect() as conn:
+    s = text("select * from jobs where id = :val")
+    result = conn.execute(s, {"val": id})
+    row = result.fetchone()
 
-        # Convert the row to a dictionary using _asdict() method
-        return row._asdict()
+    if row is None:
+      return None
+
+    # Convert the row to a dictionary using _asdict() method
+    return row._asdict()
+
+
+def add_application_to_db(job_id, application):
+  with engine.connect() as conn:
+    query = text("""
+            INSERT INTO applications 
+            (job_id, full_name, email, linkedin_url, education,                 work_experience, resume_url) 
+            VALUES (:job_id, :full_name, :email, :linkedin_url,                 :education, :work_experience, :resume_url)
+            """)
+
+    params = {
+      "job_id": job_id,
+      "full_name": application['full_name'],
+      "email": application['email'],
+      "linkedin_url":
+      application.get('linkedin_url',
+                      None),  # using .get() to handle missing keys gracefully
+      "education": application.get('education', None),
+      "work_experience": application.get('work_experience', None),
+      "resume_url": application.get('resume_url', None)
+    }
+
+    conn.execute(query, params)
+
+
+#def add_application_to_db(job_id, application):
+#  with engine.connect() as conn:
+#   query = text("INSERT INTO applications (job_id, full_name, email, linkdedin_url, education, work_experience, resume_url) VALUES (: job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)" )
+
+# conn.execute(query,
+#             job_id=job_id,
+#            full_name=application['full_name'],
+#           email=application['email'],
+#           linkedin_url=application['linkedin_url'],
+#           education=application['education'],
+#          work_experience=application['work_experience'],
+#          resume_url=application['resume_url'])
--- main.py
+++ main.py
@@ -1,30 +1,44 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
-from sqlalchemy import text 
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
+from sqlalchemy import text
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_career():
-    jobs = load_jobs_from_db()
-    return render_template('home.html',
-                           jobs=jobs)
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+
 @app.route("/jobs/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  
+
   if not job:
     return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form.to_dict()
+  job = load_job_from_db(id)
   
-  return render_template('jobpage.html',
-                        job=job)
+  #store in db
+  add_application_to_db(id, data)
   
+  #send an email
+  #display an acknowledgment
+  return render_template('application_submitted.html', application=data, job=job)
+
 
 if __name__ == "__main__":
   #app.run(host='0.0.0.0', debug=True)
